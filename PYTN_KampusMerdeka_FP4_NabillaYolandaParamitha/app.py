fc030b95be9d8189f0e60f35468c16f5d4c2e18d
nabillayo <nayomimi21@gmail.com>
2022-12-16 21:58:30 +0700

    Initial Commit add FP4

---
+++ PYTN_KampusMerdeka_FP4_NabillaYolandaParamitha/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, url_for
+import numpy as np
+import pickle
+
+model = pickle.load(open("model/model_FP4.pkl", "rb"))
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def main():
+    return(render_template('main.html'))
+
+# Redirecting the API to predict the result
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    """
+    For Rendering result on HTML GUI
+    """
+    int_features = [x for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    return render_template("main.html", prediction_text="Berdasarkan analisa, pengguna masuk kedalam cluster : {}".format(output))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
